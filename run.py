045e6a39f9e3df4000713745a474c09b3fef9a6e
Salvino Modica <salvymody@gmail.com>
2023-08-27 12:08:37 +0000

    feat: add Intro function to run.py

--- run.py
+++ run.py
@@ -1,3 +1,35 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+"""
+This module contains the main game
+"""
+
+
+def intro():
+    """
+    Prints out the intro
+    """
+
+
+print("📜 Welcome to Text-Land!"
+      "\n\nIn a realm where words wield power and choices shape destinies,"
+      " you find yourself at the crossroads of fate."
+      "\n"
+      "A mysterious world filled with unknown dangers,"
+      " captivating stories, and hidden treasures, beckons you."
+      "\n"
+      "\nAs you embark on this epic journey, remember: every choice matters!"
+      "\nYour decisions will carve out your path, lead you to treasures,"
+      " pit you against formidable foes, and present riddles that"
+      " challenge your intellect."
+      "\n\nBut fear not, for in Text-Land, even the most ordinary adventurers"
+      " can become legends."
+      "\nDo you have what it takes to conquer the"
+      " challenges, decipher the mysteries, and emerge as the hero"
+      " of Text-Land?"
+      "\n"
+      "\nOr will you be its Doom?"
+      "\n\n1. 🛡️  Enter your name, steady yourself and begin your adventure."
+      "\n2. 📘 Read about the rules of Text-Land."
+      "\n3. 🚪 Exit to the real world.")
+
+
+intro()
