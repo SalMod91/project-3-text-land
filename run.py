aeec1f35420281f02d5bc734e67017bf3561638d
Salvino Modica <salvymody@gmail.com>
2023-09-01 09:15:38 +0000

    refactor:(run.py) updated story content

--- run.py
+++ run.py
@@ -20,17 +20,17 @@ def get_choice(max_choice):
     """
     while True:
         try:
-            choice = int(input("\nEnter your choice: "))
+            choice = int(input("\n Enter your choice: "))
 
             if 1 <= choice <= max_choice:
                 print_horizontal_line()
                 return choice
             else:
                 print()  # Prints an empty line for separation
-                print(f"Please enter a number between 1 and {max_choice}.")
+                print(f" Please enter a number between 1 and {max_choice}.")
         except ValueError:
             print()  # Prints an empty line for separation
-            print("Invalid input! Please enter a number.")
+            print(" Invalid input! Please enter a number.")
 
 
 def rules():
@@ -129,45 +129,6 @@ def use_potion():
         else:
             print()  # Prints an empty line for separation
             print(f"You don't have any {selected_potion} left!")
-
-
-def intro():
-    """
-    Provides the initial game introduction to the player.
-
-    Prompts the player with the choice of starting the game,
-    reading the rules or exiting the game.
-    """
-    reset_game(player, enemy)
-    print("📜 Welcome to Text-Land!"
-          "\n\nIn a realm where words wield power and choices shape destinies,"
-          " you find yourself at the crossroads of fate."
-          "\nA mysterious world filled with unknown dangers,"
-          " captivating stories, and hidden treasures beckons you."
-          "\n\nAs you embark on this epic journey, remember:"
-          " every choice matters!"
-          "\nYour decisions will carve out your path, lead you to treasures, "
-          "pit you against formidable foes, and present riddles that challenge"
-          " your intellect."
-          "\n\nBut fear not, in Text-Land, even the most ordinary adventurers"
-          " can become legends."
-          "\nDo you have what it takes to conquer the challenges, decipher the"
-          " mysteries, and emerge as the hero of Text-Land?"
-          "\n\nOr will you be its Doom?"
-          "\n\n1. 🛡️  Enter your name and begin your adventure."
-          "\n2. 📘 Read about the rules of Text-Land."
-          "\n3. 🚪 Exit to the real world.")
-
-    choice = get_choice(3)
-    if choice == 1:
-        player_name_choice()
-
-    elif choice == 2:
-        rules()
-
-    else:
-        print("Not everyone is suited to be a Hero \U0001F44E Goodbye!")
-        exit()
 
 
 enemy = {
@@ -315,7 +276,8 @@ class Combat:
         """
         Actions to take when the player defeats the enemy in combat
         """
-        print(f"You have defeated the {self.enemy['Name']}!")
+        print_horizontal_line()
+        print(f" You have defeated the {self.enemy['Name']}!")
         self.handle_loot()
 
     def handle_loot(self):
@@ -323,12 +285,11 @@ class Combat:
         Handles loot
         """
         self.player["Stats"]["Gold"] += self.enemy["Loot"]["Gold"]
-        print(f"You got {self.enemy['Loot']['Gold']} Gold!")
+        print(f" You got {self.enemy['Loot']['Gold']} Gold!")
 
         for loot_name in self.enemy["Loot"]["Items"]:
             item_detail = item_database.get(loot_name)
             if item_detail:
-                print("Handling Loot")
                 self.equip_item(item_detail)
 
     def equip_item(self, item_detail):
@@ -336,6 +297,7 @@ class Combat:
         Equips automatically better equipment
         """
         item_type = item_detail["Type"]
+        item_name = item_detail["Name"]
 
         currently_equipped = self.player["Equipment"].get(item_type, None)
 
@@ -345,9 +307,8 @@ class Combat:
             and the old equipped one if any is equipped
             """
             change = {}
-            print("Calculating...")
             for stat, value in new_item.items():
-                if stat != "Type":
+                if stat != "Type" and "Name":
                     if old_item:
                         change[stat] = value - old_item.get(stat, 0)
                     else:
@@ -363,15 +324,16 @@ class Combat:
                 self.player["Equipment"][item_type] = item_detail
                 for stat, change in stat_change.items():
                     self.player["Stats"][stat] += change
-                print(f"Equipped {item_detail}")
+                print(f"You found and equipped {item_name}")
             else:
-                print(f"Found {item_detail}, but current equipment is better!")
+                print(f" Found {item_detail}, but current"
+                      " equipment is better!")
         else:
             self.player["Equipment"][item_type] = item_detail
             for stat, value in item_detail.items():
-                if stat != "Type":
+                if stat != "Type" and "Name":
                     self.player["Stats"][stat] += value
-            print(f"Equipped {item_detail['Type']}!")
+            print(f" Equipped {item_detail['Type']}!")
 
     def display_combat_info(self):
         """
@@ -379,55 +341,55 @@ class Combat:
         """
         print("PLAYER\t\tENEMY")
         print_horizontal_line()
-        print(f"Name: {self.player['Stats']['Name']}\tName: "
+        print(f" Name: {self.player['Stats']['Name']}\tName: "
               f"{self.enemy['Name']}")
-        print(f"HP: {self.player['Stats']['Max HP']}\t\tHP: "
+        print(f" HP: {self.player['Stats']['Max HP']}\t\tHP: "
               f"{self.enemy['Max HP']}")
-        print(f"Attack: {self.player['Stats']['Atk']}\tAttack: "
+        print(f" Attack: {self.player['Stats']['Atk']}\tAttack: "
               f"{self.enemy['Atk']}")
-        print(f"Defence: {self.player['Stats']['Def']}\tDefence: "
+        print(f" Defence: {self.player['Stats']['Def']}\tDefence: "
               f"{self.enemy['Def']}")
-        print(f"Crit: {self.player['Stats']['Crit']}\t\tCrit: "
+        print(f" Crit: {self.player['Stats']['Crit']}\t\tCrit: "
               f"{self.enemy['Crit']}")
-        print(f"\t\tRun: {self.enemy['Run']}")
+        print(f" \t\tRun: {self.enemy['Run']}")
 
     def player_combat_defeat(self):
         """
         Actions to take when the player is defeated in combat
         """
-        print(f"The {self.enemy['Name']} hit you with a killing blow!")
+        print(f" The {self.enemy['Name']} hit you with a killing blow!")
         player_defeat()
 
     def combat_loop(self):
         """
         Handles combat
         """
-        print(f"======== COMBAT VS {self.enemy['Name'].upper()} ========")
+        print(f" ======== COMBAT VS {self.enemy['Name'].upper()} ========")
         while (self.player['Stats']['Current HP'] > 0 and
                self.enemy['Current HP'] > 0):
-            print("\nENEMY IMAGE")
+            print("\n ENEMY IMAGE")
             print(f"\n{self.player['Stats']['Name']} HP:"
                   f" {self.player['Stats']['Current HP']}"
                   f"/{self.player['Stats']['Max HP']} |"
                   f" {self.enemy['Name']} HP:"
                   f" {self.enemy['Current HP']}"
                   f"/{self.enemy['Max HP']}")
-            print("\n1. Attack"
-                  "\n2. Item"
-                  "\n3. Info"
-                  "\n4. Run")
+            print("\n 1. Attack"
+                  "\n 2. Item"
+                  "\n 3. Info"
+                  "\n 4. Run")
             choice = get_choice(4)
             if choice == 1:
                 dmg_to_enemy = round((self.player['Stats']["Atk"] -
                                       self.enemy["Def"]) * dmg_roll())
                 if dmg_to_enemy > 0:
                     self.enemy['Current HP'] -= dmg_to_enemy
-                    print(f"You dealt {dmg_to_enemy} damage to the"
+                    print(f" You dealt {dmg_to_enemy} damage to the"
                           f" {self.enemy['Name']}!")
                     if self.enemy['Current HP'] <= 0:
                         self.player_combat_victory()
                 else:
-                    print("You did no damage."
+                    print(" You did no damage."
                           " The enemy's defense is too high!")
 
                 if self.enemy['Current HP'] > 0:
@@ -437,10 +399,11 @@ class Combat:
                         )
                     if dmg_to_player > 0:
                         self.player["Stats"]["Current HP"] -= dmg_to_player
-                        print(f"The {self.enemy['Name']} dealt {dmg_to_player}"
+                        print(f" The {self.enemy['Name']}"
+                              " dealt {dmg_to_player}"
                               " damage to you!")
                     else:
-                        print(f"The {self.enemy['Name']} did no damage."
+                        print(f" The {self.enemy['Name']} did no damage."
                               " Your defense is too high!")
 
                     if self.player['Stats']['Current HP'] <= 0:
@@ -452,10 +415,11 @@ class Combat:
                                           ["Stats"]["Def"]) * dmg_roll())
                     if dmg_to_player > 0:
                         self.player["Stats"]["Current HP"] -= dmg_to_player
-                        print(f"The {self.enemy['Name']} dealt {dmg_to_player}"
+                        print(f" The {self.enemy['Name']}"
+                              " dealt {dmg_to_player}"
                               " damage to you!")
                     else:
-                        print(f"The {self.enemy['Name']} did no damage."
+                        print(f" The {self.enemy['Name']} did no damage."
                               " Your defense is too high!")
 
                     if self.player['Stats']['Current HP'] <= 0:
@@ -466,20 +430,21 @@ class Combat:
 
             else:
                 if player_run_away(self.enemy['Run']):
-                    print("You successfully ran away!")
+                    print(" You successfully ran away!")
                     return  # Exits the combat loop
 
                 else:
-                    print(f"The {self.enemy['Name']} blocked your way."
+                    print(f" The {self.enemy['Name']} blocked your way."
                           " You couldn't escape.")
                     dmg_to_player = round((self.enemy["Atk"] - self.player
                                           ["Stats"]["Def"]) * dmg_roll())
                     if dmg_to_player > 0:
                         self.player["Stats"]["Current HP"] -= dmg_to_player
-                        print(f"The {self.enemy['Name']} dealt {dmg_to_player}"
+                        print(f" The {self.enemy['Name']}"
+                              " dealt {dmg_to_player}"
                               " damage to you!")
                     else:
-                        print(f"The {self.enemy['Name']} did no damage."
+                        print(f" The {self.enemy['Name']} did no damage."
                               " Your defense is too high!")
 
                     if self.player['Stats']['Current HP'] <= 0:
@@ -622,29 +587,94 @@ def print_player_info_menu(player):
     print(f"Hands: {hands_name}\t Defence: +{hands_stat}")
 
 
+def intro():
+    """
+    Provides the initial game introduction to the player.
+
+    Prompts the player with the choice of starting the game,
+    reading the rules or exiting the game.
+    """
+    reset_game(player, enemy)
+    print(" 📜 Welcome to Text-Land!"
+          "\n In a realm where words wield power and choices shape destinies,"
+          " you find yourself at the crossroads of fate."
+          "\n A mysterious world filled with unknown dangers,"
+          " captivating stories, and hidden treasures beckons you."
+          "\n As you embark on this epic journey, remember:"
+          " every choice matters!"
+          "\n Your decisions will carve out your path, lead you to treasures, "
+          "pit you against formidable foes, and present riddles that challenge"
+          " your intellect."
+          "\n But fear not, in Text-Land, even the most ordinary adventurers"
+          " can become legends."
+          "\n Do you have what it takes to conquer the challenges,"
+          " decipher the"
+          " mysteries, and emerge as the hero of Text-Land?"
+          "\n Or will you be its Doom?"
+          "\n 1. 🛡️  Enter your name and begin your adventure."
+          "\n 2. 📘 Read about the rules of Text-Land."
+          "\n 3. 🚪 Exit to the real world.")
+
+    choice = get_choice(3)
+    if choice == 1:
+        player_name_choice()
+
+    elif choice == 2:
+        rules()
+
+    else:
+        print("Not everyone is suited to be a Hero \U0001F44E Goodbye!")
+        exit()
+
+
 def first_scene():
     """
     This is going to be the first scene
     """
-    print("\nThis is the first scene")
+    print(" Blinking awake, you find yourself by a serene lake."
+          " How did you end up here? Your memory's a blur."
+          "\n As you're piecing things together, a distant"
+          " scream of help shatters the tranquility."
+          "\n Something's not right!"
+          "\n What will you do?")
     while True:
-        print("\n1. Go towards the screams\n"
-              "2. Player Info")
+        print("\n 1. Run towards the screams"
+              "\n 2. The screams dont concern you."
+              " You rather go back to sleep."
+              "\n 3. Player Info")
 
-        choice = get_choice(2)
+        choice = get_choice(3)
         if choice == 1:
             battle = Combat(player, enemy["Goblin"])
             battle.combat_loop()
             break
-        else:
+        elif choice == 2:
+            first_scene_game_over()
+        elif choice == 3:
             print_player_info_menu(player)
+
+
+def first_scene_game_over():
+    """
+    Calls the game over screen if the player chooses
+    the second option of the first scene
+    """
+    print(" You ignored the screams of help and went back to sleep..."
+          "\n Zzz...Zzz..Zz AAGH!"
+          "\n You suddenly wake up to a stinging pain in your chest!"
+          "\n You see a Goblin in front of you grinning"
+          " with a malicious smile."
+          "\n As your eyes wander you notice the goblin's"
+          " blade in your heart.."
+          "\n You go back to sleep....this time forever......")
+    player_defeat()
 
 
 def second_scene():
     """
     This is going to be the second scene
     """
-    print("\nThs is the second scene")
+    print("\n Ths is the second scene")
     while True:
         print("(1)"
               "2")
